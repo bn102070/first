dec23465ae3f32efecc04d440bc48c3831db4baf
bn102070 <bn102070@hust.edu.tw>
2020-07-11 02:30:46 +0800

    444

--- js/nav.js
+++ js/nav.js
@@ -5,6 +5,9 @@ var nav = document.querySelector(".navBar")
 
 burgar.onclick = function () { //Loading效果 與 Aos初始化
 
+    //按鈕變x
+    burgar.classList.toggle("change");
+
     if (nav.className.indexOf('nav_active') == -1) {
         nav.classList.add("nav_active")
         $('body').addClass('scrollY_hidden')
